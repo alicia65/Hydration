9bed6534f5783be89b1cb6a0f45a28c1e5478b60
alicia65 <alicia65>
2020-02-19 23:55:20 -0600

    add comments

--- Hydration/Form1.cs
+++ Hydration/Form1.cs
@@ -10,16 +10,21 @@ using System.Windows.Forms;
 
 namespace Hydration
 {
+    /*
+     This program is using numericUpDown control to select number of glasses of water. 
+    In addition, label control displays color with text message to show level of safety 
+    color to drink. For example, red color is drinking too much. Whereas, gray color is not drinking enough. 
+    */
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
         }
-
+        
         private void numericUpDown1_ValueChanged(object sender, EventArgs e)
         {
-            string glassOfWater = lblResult.Text;
+            string glassOfWater = lblResult.Text;// Initilize label result to pass a string to glass of water variable
 
             if (glassOfWater.Length > 12)
             {
@@ -27,10 +32,10 @@ namespace Hydration
                 lblResult.BackColor = Color.Red;
                 this.BackColor = Color.Green;
             }
-            else if (glassOfWater.Length > 9 && glassOfWater.Length < 12)
+            else if (glassOfWater.Length > 9 && glassOfWater.Length < 12)//Using && operator to compare to conditions
             {
-                lblResult.Text = "Awesome";
-                lblResult.BackColor = Color.DarkBlue;
+                lblResult.Text = "Awesome";//Label result displays text 
+                lblResult.BackColor = Color.DarkBlue;//Label result shows color
                 this.BackColor = Color.Green;
             }
             else if (glassOfWater.Length >= 5 && glassOfWater.Length <= 8) 
